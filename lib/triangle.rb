2ef07702eaabcf7298f2038fb846e315093891c6
Zach <zawood11@gmail.com>
2022-03-03 22:29:15 -0500

    commit tree 946e6fa79781ffaf942245755e5d884f3e6a86d4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind
+    a, b, c = [@first, @second, @third].sort
+
+    if a <= 0 || a + b <= c
+      begin
+        raise TriangleError
+      end
+    elsif a == c
+      :equilateral
+    elsif a == b || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
 end
+
+#  t1 = Triangle.new(0, 0, 0)
+#  t1.kind
